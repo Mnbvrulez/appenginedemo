12901f7441d7221ed20a1edbdf92c0769693d218
Mnbvrulez <jane@Janes-MacBook-Air.local>
2014-07-08 15:31:48 +1000

    form appears

---
+++ jcjcfret/js/account.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+
+	$("#account_edit_button").click(function(event) {
+		console.log("this button is working");
+		var edit_form = document.getElementById("edit_profile");
+		edit_form.style.visibility = "hidden";
+        
+	});
+});
